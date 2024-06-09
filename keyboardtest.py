c39d98654c8e3c2df5deb3bf421bf6e5635813c7
Mylo <joachim.fevre@gmail.com>
2024-06-09 02:35:31 +0200

    finished

--- keyboardtest.py
+++
@@ -1,7 +0,0 @@
-import keyboard
-
-while True:
-	if keyboard.is_pressed('left'):
-		print('pressed')
-	else:
-		print("unpressed")
--- packages/global_mod.py
+++ packages/global_mod.py
@@ -3,7 +3,7 @@ class Global():
 	screen_height = 900
 	screen_width = 1600
 
-	bullet_min_speed, bullet_max_speed = 1,3
+	bullet_min_speed, bullet_max_speed = 2,4
 	player_speed = 2
 
 	bullet_size = 100
@@ -12,5 +12,5 @@ class Global():
 	bullet_color = (255, 0, 0)
 	player_color = (0, 0, 255)
 
-	n_bullet = 5 #Be carfull with that
+	n_bullet = 6 #Be carfull with that
 
